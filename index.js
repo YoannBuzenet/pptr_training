5a93316015544b46c8aa781dd9effff4721af7b8
Yoann Buzenet <ybuzenet@gmail.com>
2022-05-10 19:22:24 +0200

    base folder

---
+++ index.js
@@ -0,0 +1,14 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+  const page = await browser.newPage();
+  await page.goto("https://www.lemonde.fr");
+  //await page.screenshot({ path: "example.png" });
+
+  await page.waitForTimeout(5000);
+
+  await browser.close();
+})();
